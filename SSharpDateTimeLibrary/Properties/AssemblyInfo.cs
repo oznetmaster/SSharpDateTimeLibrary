7671aee17137ad92615380faadab58ce10c1402c
oznetmaster <ncolvin@marvelous.com>
2015-01-04 12:17:29 +0800

    Initial Commit

---
+++ SSharpDateTimeLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle ("SSharpDateTimeLibrary")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSharpDateTimeLibrary")]
+[assembly: AssemblyCopyright ("Copyright ©  2014")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+
